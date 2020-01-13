3de444b75c5872a6e2e34816ed31ba790c678eaf
olivajames110 <olivajames110@gmail.com>
2020-01-13 17:09:31 -0500

    Added partySizeRow folder

---
+++ src/components/cattery/partySizeAvailability/partySizeRow/partySizeRow.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './css/partySizeRow.css';
+
+const PartySizeRow = () => {
+	return (
+		<div id="party-size1" className="party-size-wrapper">
+			<div className="party-size">1</div>
+			<div className="next-available-time">Check</div>
+		</div>
+	);
+};
+
+export default PartySizeRow;
