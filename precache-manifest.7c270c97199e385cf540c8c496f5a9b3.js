f272ed61dbe8e1b5f1fee96e08d63d1b50cccf9a
olivajames110 <olivajames110@gmail.com>
2020-01-16 09:10:01 -0500

    Updates

---
+++ precache-manifest.7c270c97199e385cf540c8c496f5a9b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "432b1be147a301db3da04a0a145f8fe9",
+    "url": "/cattery/index.html"
+  },
+  {
+    "revision": "80f8e517769e518179fc",
+    "url": "/cattery/static/css/main.79913c82.chunk.css"
+  },
+  {
+    "revision": "174dab6dab22c777ecdc",
+    "url": "/cattery/static/js/2.90ca1326.chunk.js"
+  },
+  {
+    "revision": "3b4e6b57b14ffa1a3099eb73287cf0e5",
+    "url": "/cattery/static/js/2.90ca1326.chunk.js.LICENSE"
+  },
+  {
+    "revision": "80f8e517769e518179fc",
+    "url": "/cattery/static/js/main.885eaaae.chunk.js"
+  },
+  {
+    "revision": "a32ab032feebd8f67f8f",
+    "url": "/cattery/static/js/runtime-main.d217457e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cattery/precache-manifest.cf3a4f250c2c33908cfa0847cdc67900.js"
+  "/cattery/precache-manifest.7c270c97199e385cf540c8c496f5a9b3.js"
 );
 
 self.addEventListener('message', (event) => {
