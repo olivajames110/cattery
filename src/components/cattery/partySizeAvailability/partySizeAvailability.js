fc683ce112c7fb1001ae6461330c8fee2a9e5fb2
Jimmy Oliva <olivajames110@gmail.com>
2020-01-28 20:56:56 -0500

    Refactor

--- src/components/cattery/partySizeAvailability/partySizeAvailability.js
+++ src/components/cattery/partySizeAvailability/partySizeAvailability.js
@@ -1,46 +1,51 @@
-import React, { Component } from 'react';
-import './css/partySizeAvailability.css';
-import PartySizeRow from './partySizeRow/partySizeRow';
-const PartySizeAvailability = (props) => {
-	//   let partyRows = function() {
-	//     let partyRows = (
-	//       <div className="containr">
-	//         <PartySizeRow partySize="1" />
-	//         <PartySizeRow partySize="1" />
-	//         <PartySizeRow partySize="1" />
-	//       </div>
-	//     );
-	let filtedParties = () => {
-		let newParties = props.parties.filter((party) => {
-			return party.rowNum === 1 || party.isUpcomingReservation === true;
-		});
+import React, { Component } from "react";
+import "./css/partySizeAvailability.css";
+import PartySizeRow from "./partySizeRow/partySizeRow";
+import * as moment from "moment";
+const PartySizeAvailability = props => {
+  //   let partyRows = function() {
+  //     let partyRows = (
+  //       <div className="containr">
+  //         <PartySizeRow partySize="1" />
+  //         <PartySizeRow partySize="1" />
+  //         <PartySizeRow partySize="1" />
+  //       </div>
+  //     );
+  let filteredParties = () => {
+    let newParties = props.parties.filter(party => {
+      return (
+        (party.rowNum === 1 || party.isUpcomingReservation === true) &&
+        party.isOverdue === false
+      );
+    });
 
-		console.dir(newParties);
-		// return newParties;
-	};
+    console.dir(newParties);
+    // console.log(moment("8:49 PM")._locale);
+    // return newParties;
+  };
 
-	let partyRows = Array.from(Array(15)).map((x, i) => (
-		<PartySizeRow
-			currentTime={props.currentTime}
-			currentOccupancy={props.currentOccupancy}
-			parties={props.parties}
-			partySize={i + 1}
-			key={Math.random()}
-		/>
-	));
+  let partyRows = Array.from(Array(15)).map((x, i) => (
+    <PartySizeRow
+      currentTime={props.currentTime}
+      currentOccupancy={props.currentOccupancy}
+      parties={props.parties}
+      partySize={i + 1}
+      key={Math.random()}
+    />
+  ));
 
-	// for (let i = 0; i <= 15; i++) {
-	//   //   console.log("Party: " + i);
-	//   return <PartySizeRow partySize="1" />;
-	// }
+  // for (let i = 0; i <= 15; i++) {
+  //   //   console.log("Party: " + i);
+  //   return <PartySizeRow partySize="1" />;
+  // }
 
-	return (
-		<div id="party-size-availability-container">
-			<h2>Walk-In Availability</h2>
-			{partyRows}
-			{filtedParties()}
-		</div>
-	);
+  return (
+    <div id="party-size-availability-container">
+      <h2>Walk-In Availability</h2>
+      {partyRows}
+      {filteredParties()}
+    </div>
+  );
 };
 
 export default PartySizeAvailability;
--- src/components/cattery/partySizeAvailability/partySizeRow/partySizeRow.js
+++ src/components/cattery/partySizeAvailability/partySizeRow/partySizeRow.js
@@ -1,88 +1,90 @@
-import React, { Component } from 'react';
-import { checkMark } from '../../../../utils/icons/icons';
-import './css/partySizeRow.css';
-import { render } from '@testing-library/react';
+import React, { Component } from "react";
+import { checkMark } from "../../../../utils/icons/icons";
+import "./css/partySizeRow.css";
+import { render } from "@testing-library/react";
 
 class PartySizeRow extends Component {
-	state = {
-		isAvailable       : false,
-		nextAvailableTime : '2:45'
-	};
+  state = {
+    isAvailable: false,
+    nextAvailableTime: "2:45"
+  };
 
-	componentWillMount() {
-		this.checkIfAvailable();
-	}
+  componentWillMount() {
+    this.checkIfAvailable();
+  }
 
-	checkIfAvailable = () => {
-		let currentParty = this.props.partySize;
-		let currentParties = this.props.currentOccupancy;
-		let listOfParties = this.props.parties;
+  checkIfAvailable = () => {
+    let currentParty = this.props.partySize;
+    let currentParties = this.props.currentOccupancy;
+    let listOfParties = this.props.parties;
 
-		if (currentParty < currentParties) {
-			// console.log('Current: ' + currentParty);
-			// console.log('End: ' + currentParties);
-			// console.log('Parties: ' + this.props.currentOccupancy);
-			this.setState({
-				isAvailable : true
-			});
-		} else {
-			this.setState({
-				isAvailable : false
-			});
-		}
-	};
+    if (currentParty < currentParties) {
+      // console.log('Current: ' + currentParty);
+      // console.log('End: ' + currentParties);
+      // console.log('Parties: ' + this.props.currentOccupancy);
+      this.setState({
+        isAvailable: true
+      });
+    } else {
+      this.setState({
+        isAvailable: false
+      });
+    }
+  };
 
-	getNextAvailableTime = (partySize) => {
-		let nextAvailableTime;
-		let spotsRemaining = 15 - this.props.currentOccupancy;
-		let exitedGuests = spotsRemaining + this.props.parties[0].numberInParty;
-		let firstPartyEndTime = this.props.parties[0].timeEnd;
+  getNextAvailableTime = partySize => {
+    let nextAvailableTime;
+    let spotsRemaining = 15 - this.props.currentOccupancy;
+    let exitedGuests = spotsRemaining + this.props.parties[0].numberInParty;
+    let firstPartyEndTime = this.props.parties[0].timeEnd;
 
-		console.log(`spotsRemaining: ${spotsRemaining}`);
-		console.log(`--------partySize: ${this.props.partySize}`);
-		console.log(`-----exitedGuests: ${exitedGuests}`);
+    // console.log(`spotsRemaining: ${spotsRemaining}`);
+    // console.log(`--------partySize: ${this.props.partySize}`);
+    // console.log(`-----exitedGuests: ${exitedGuests}`);
 
-		// if (this.props.partySize <= exitedGuests) {
-		// 	return firstPartyEndTime;
-		// } else {
-		// 	return 'Later';
-		// }
+    // if (this.props.partySize <= exitedGuests) {
+    // 	return firstPartyEndTime;
+    // } else {
+    // 	return 'Later';
+    // }
 
-		// do {
+    // do {
 
-		// }
-		// while(this.props.partySize <= exitedGuests)
+    // }
+    // while(this.props.partySize <= exitedGuests)
 
-		for (let i = 0; i < this.props.parties.length; i++) {
-			if (this.props.partySize <= exitedGuests) {
-				nextAvailableTime = this.props.parties[i].timeEnd;
-				break;
-			} else {
-				exitedGuests += this.props.parties[i].numberInParty;
-			}
-		}
+    for (let i = 0; i < this.props.parties.length; i++) {
+      if (this.props.partySize <= exitedGuests) {
+        nextAvailableTime = this.props.parties[i].timeEnd;
+        break;
+      } else {
+        exitedGuests += this.props.parties[i].numberInParty;
+      }
+    }
 
-		return nextAvailableTime;
-	};
+    return nextAvailableTime;
+  };
 
-	render() {
-		const { partySize } = this.props;
+  render() {
+    const { partySize } = this.props;
 
-		return (
-			<div
-				onClick={() => {
-					this.getNextAvailableTime();
-				}}
-				id="party-size1"
-				className="party-size-wrapper"
-			>
-				<div className="party-size">{partySize}</div>
-				<div className="next-available-time">
-					{partySize <= 15 - this.props.currentOccupancy ? checkMark : this.getNextAvailableTime(partySize)}
-				</div>
-			</div>
-		);
-	}
+    return (
+      <div
+        onClick={() => {
+          this.getNextAvailableTime();
+        }}
+        id="party-size1"
+        className="party-size-wrapper"
+      >
+        <div className="party-size">{partySize}</div>
+        <div className="next-available-time">
+          {partySize <= 15 - this.props.currentOccupancy
+            ? checkMark
+            : this.getNextAvailableTime(partySize)}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default PartySizeRow;
