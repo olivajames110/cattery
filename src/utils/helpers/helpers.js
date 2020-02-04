4db0ad72c4e3609236113f6597a6f84603d4a61f
Jimmy Oliva <olivajames110@gmail.com>
2020-02-03 21:32:17 -0500

    code refactoring

--- src/utils/helpers/helpers.js
+++ src/utils/helpers/helpers.js
@@ -1,59 +1,90 @@
+import * as moment from "moment";
 export function formatAMPM(date) {
-	let hours = date.getHours();
-	let minutes = date.getMinutes();
-	let ampm = hours >= 12 ? 'pm' : 'am';
-	hours = hours % 12;
-	hours = hours ? hours : 12; // the hour '0' should be '12'
-	minutes = minutes < 10 ? '0' + minutes : minutes;
-	let currentTime = hours + ':' + minutes + '' + ampm;
-	return currentTime;
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let ampm = hours >= 12 ? "pm" : "am";
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  let currentTime = hours + ":" + minutes + "" + ampm;
+  return currentTime;
 }
 
 export function checkIfMultiple(props) {}
 
 export function checkifOverlap(props) {}
 
-
 //creates a new sorted array only containing specificed key
 export function sortArrayByKey(party, key, value) {
-	let partiesList;
-	partiesList = party.filter((party) => {
-		return party[key] === value;
-	});
-	let sortedPartiesList = partiesList.sort(function (p1, p2) {
-		if (p1.times.timeStamp >= p2.times.timeStamp) {
-			return 1;
-		} else {
-			// moves larger to the top of array
-			return -1;
-		}
-	});
+  let partiesList;
+  partiesList = party.filter(party => {
+    return party[key] === value;
+  });
+  let sortedPartiesList = partiesList.sort(function(p1, p2) {
+    if (p1.times.timeStamp >= p2.times.timeStamp) {
+      return 1;
+    } else {
+      // moves larger to the top of array
+      return -1;
+    }
+  });
 
-	return sortedPartiesList;
+  return sortedPartiesList;
 }
 
 //sorts array ---> returns sorted array in select
 export function sortPartyArray(party, rowNum, seperateByRow) {
-	let partiesList;
+  let partiesList;
 
-	if (seperateByRow) {
-		partiesList = party.filter((party) => {
-			return party.rowNum === rowNum;
-		});
-	} else {
-		partiesList = party;
-	}
+  if (seperateByRow) {
+    partiesList = party.filter(party => {
+      return party.rowNum === rowNum;
+    });
+  } else {
+    partiesList = party;
+  }
 
-	let sortedPartiesList = partiesList.sort(function (p1, p2) {
-		if (p1.times.timeStamp >= p2.times.timeStamp) {
-			return 1;
-		} else {
-			// moves larger to the top of array
-			return -1;
-		}
-	});
+  let sortedPartiesList = partiesList.sort(function(p1, p2) {
+    if (p1.times.timeStamp >= p2.times.timeStamp) {
+      return 1;
+    } else {
+      // moves larger to the top of array
+      return -1;
+    }
+  });
+  return sortedPartiesList;
+}
 
-	return sortedPartiesList;
+export function handleGetTimes() {
+  let times = {
+    minute: moment().minute(),
+    hour: moment().hour(),
+    start: moment().format("h:mm A"),
+    end: moment()
+      .add("hours", 1)
+      .format("h:mm A")
+  };
 
+  return times;
+}
 
+export function handleUpdateTimes(id) {
+  let newTimes = this.handleGetTimes();
+  let filteredParty = this.getFilteredParty(id);
+  filteredParty[0].times.start = newTimes.times.start;
+  filteredParty[0].times.end = newTimes.times.end;
+}
+
+//Returns party
+// export function getFilteredParty(id) {
+//   let filteredParty = this.state.parties.filter(party => {
+//     return party.id === id;
+//   });
+
+//   return filteredParty;
+// }
+
+export function updatePartyData(id, targetKey, value) {
+  let filteredParty = this.getFilteredParty(id);
+  filteredParty[0][targetKey] = value;
 }
